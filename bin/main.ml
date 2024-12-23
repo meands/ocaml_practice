def87c210b8cb5d2981996d2910ca440e1741870
Mandy <mandyyj1207@gmail.com>
2024-12-23 14:11:34 +0000

    this is so hard T_T

--- bin/main.ml
+++ bin/main.ml
@@ -17,3 +17,42 @@ let rec find_nth n = function
   | [] -> None
   | x::xs -> if n = 0 then Some x else find_nth (n-1) xs  
 
+(* Find the number of elements of a list. *)
+let rec length list = 
+  let rec inc c = function
+    | [] -> c
+    | _::xs -> inc (c+1) xs
+  in inc 0 list
+
+(* Reverse a list. *)
+let rev list = 
+  let rec aux r = function
+    | [] -> r
+    | x::xs -> aux (x::r) xs
+  in aux [] list
+
+(* Find out whether a list is a palindrome. *)
+let is_palindrome list = rev list = list
+
+(* Run-length encoding *)
+let encode list = 
+  let rec aux count acc = function
+    | [] -> []
+    | [x] -> (count, x)::acc
+    | a::(b::c) -> if a = b then aux (count+1) acc c else aux 0 ((count+1, a)::acc) c
+in aux 0 [] list
+
+(* Modify the result of the previous problem in such a way that if an element has no duplicates it is simply copied into the result list. 
+  Only elements with duplicates are transferred as (N E) lists. *)
+type 'a rle =
+  | One of 'a
+  | Many of int * 'a
+let encode_modified list = 
+  let create_element count el = 
+    if count = 1 then One el
+    else Many (count, el) in
+  let rec aux count acc = function
+    | [] -> []
+    | [x] -> (create_element count x)::acc
+    | a::(b::c) -> if a = b then aux (count+1) acc c else aux 0 ((create_element (count+1) a)::acc) c
+in aux 0 [] list
