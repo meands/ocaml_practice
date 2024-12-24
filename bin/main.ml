0516d6dc7d69538940e26791ebe8a37a439399f0
Mandy <mandyyj1207@gmail.com>
2024-12-24 14:52:21 +0000

    more beginner

--- bin/main.ml
+++ bin/main.ml
@@ -56,3 +56,42 @@ let encode_modified list =
     | [x] -> (create_element count x)::acc
     | a::(b::c) -> if a = b then aux (count+1) acc c else aux 0 ((create_element (count+1) a)::acc) c
 in aux 0 [] list
+
+(* Duplicate the elements of a list. *)
+let rec duplicate = function 
+  | [] -> [] 
+  | x::xs -> x::x::duplicate xs
+
+(* Split a list into two parts; the length of the first part is given. 
+  If the length of the first part is longer than the entire list, then the first part is the list and the second part is empty. *)
+let split list cnt = 
+  let rec aux acc n = function 
+    | [] -> (List.rev acc, [])
+    | x::xs as l -> if n = 0 then (List.rev acc, l) else aux (x::acc) (n-1) xs
+in aux [] cnt list
+
+(* Remove the K'th element from a list. *)
+let rec remove_at k = function
+  | [] -> []
+  | x::xs -> if k = 0 then xs else x::remove_at (k-1) xs
+
+(* Insert an Element at a Given Position Into a List *)
+let rec insert_at el n = function
+  | [] -> [el]
+  | x::xs as l -> if n = 0 then el::l else x::insert_at el (n-1) xs
+
+(* Create a List Containing All Integers Within a Given Range *)
+let range a b =
+  let rec aux a b = 
+    if a > b then [] else a::aux (a+1) b
+in if a > b then List.rev (aux b a) else aux a b
+
+(* Lotto: Draw N Different Random Numbers From the Set 1..M *)
+
+(* Generate a Random Permutation of the Elements of a List *)
+
+(* Determine Whether Two Positive Integer Numbers Are Coprime *)
+
+(* Compare the Two Methods of Calculating Euler's Totient Function *)
+
+(* Given a range of integers by its lower and upper limit, construct a list of all prime numbers in that range. *)
